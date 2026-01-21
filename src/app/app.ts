d413a8e98296858ad04ff7b7ad9694d25de445d9
RayJhonatta <147754334+RayJhonatta@users.noreply.github.com>
2026-01-20 22:46:48 -0300

    feat: Creating the login screen, adjusting the CSS and TS.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { LoginScreen } from './components/login-screen/login-screen';
 
+
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',   
   imports: [LoginScreen],
   templateUrl: './app.html',
   styleUrl: './app.css'
