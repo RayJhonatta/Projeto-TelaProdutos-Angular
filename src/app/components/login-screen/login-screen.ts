600e55d4eb37b1b43bbdf745de044bd9c6201981
RayJhonatta <147754334+RayJhonatta@users.noreply.github.com>
2026-02-28 00:00:35 -0300

    feat: Adjustments to mobile screen responsiveness

--- src/app/components/login-screen/login-screen.ts
+++ src/app/components/login-screen/login-screen.ts
@@ -12,46 +12,5 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./login-screen.css'],
 })
 export class LoginScreen {
-
-  private formBuilder =  inject(FormBuilder);
-  private snackBar = inject(MatSnackBar);
-
-  dataForm = this.formBuilder.group({
-    username: ['', [Validators.required]],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-  });
-  
-  onSubmit() {
-    if(this.dataForm.invalid) {
-      this.snackBar.open('Formulário inválido! Verifique os campos.', 'Fechar', {
-        duration: 3000,
-        panelClass: ['error-snackbar']
-      });
-      return;
-    } else {
-      this.snackBar.open('Login realizado com sucesso!', 'Fechar', {
-        duration: 3000,
-        panelClass: ['success-snackbar']
-      });
-      this.dataForm.reset();
-    }
-  } 
-
-  showPassword: boolean = false;
-
-  togglePassword() {
-    const eye = document.querySelector('eye');
-    const input = document.querySelector('#password') as HTMLInputElement;
-
-    if (this.showPassword) {
-      this.showPassword = false;
-      if (eye) eye.classList.remove('visible');
-      if (input) input.type = 'password';
-    } else {
-      this.showPassword = true;
-      if (eye) eye.classList.add('visible');
-      if (input) input.type = 'text';
-    }
-  }
+  //
 }
