060b5a9cf1f95862d178966e265b8467370daa12
RayJhonatta <147754334+RayJhonatta@users.noreply.github.com>
2026-03-06 23:56:11 -0300

    feat: Setting a hidden and non-hidden password

--- src/app/components/login-screen/login-screen.ts
+++ src/app/components/login-screen/login-screen.ts
@@ -12,5 +12,18 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./login-screen.css'],
 })
 export class LoginScreen {
-  //
+
+  hidePassword: boolean = false;
+   
+  toggleSenha() {
+    const input = document.getElementById('password') as HTMLInputElement;
+
+    if (input.type === "password") {
+        input.type = "text"
+        this.hidePassword = true;
+    } else {
+      input.type = "password"
+      this.hidePassword = false;
+    }
+  }
 }
