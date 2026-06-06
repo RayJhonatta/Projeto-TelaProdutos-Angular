d978d6adff089ea2a669d1971fa9fb7e06aa6e95
RayJhonatta <147754334+RayJhonatta@users.noreply.github.com>
2026-06-06 12:35:23 -0300

    fix: adding dynamic URL identification by hostname

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
+const isProduction = window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1';
+
 export const environment = {
-  production: true,
-  apiUrl: 'https://projeto-telaprodutos-laravel-production.up.railway.app/api'
+  production: isProduction,
+  apiUrl: isProduction 
+    ? 'https://projeto-telaprodutos-laravel-production.up.railway.app/api' 
+    : 'http://127.0.0.1:8000/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
+const isProduction = window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1';
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://127.0.0.1:8000/api'
+  production: isProduction,
+  apiUrl: isProduction 
+    ? 'https://projeto-telaprodutos-laravel-production.up.railway.app/api' 
+    : 'http://127.0.0.1:8000/api'
 };
