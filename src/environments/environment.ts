b1472af21842b543291660d9e0478744b355b02d
RayJhonatta <147754334+RayJhonatta@users.noreply.github.com>
2026-07-10 11:52:06 -0300

    chore: update API URL for Render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1',
   apiUrl: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://127.0.0.1:8000/api'
-    : 'https://projeto-telaprodutos-laravel-production.up.railway.app/api'
+    : 'https://projeto-telaprodutos-laravel.onrender.com/api'
 };
