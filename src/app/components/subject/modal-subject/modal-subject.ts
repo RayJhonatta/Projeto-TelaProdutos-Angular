e5d2de1e38e5a82b6b1e9495008a85b115c03394
RayJhonatta <147754334+RayJhonatta@users.noreply.github.com>
2026-08-21 20:19:01 -0300

    feat: Creating the message container and starting the "create new subject" modal.

---
+++ src/app/components/subject/modal-subject/modal-subject.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-subject',
+    standalone: true,
+    imports: [],
+    templateUrl: './modal-subject.html',
+    styleUrl: './modal-subject.css',
+  })
+
+export class ModalSubject {
+
+}
--- src/app/components/subject/subject.ts
+++ src/app/components/subject/subject.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ModalSubject } from './modal-subject/modal-subject';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-subject',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './subject.html',
   styleUrl: './subject.css',
 })
+
 export class Subject implements OnInit {
+
+  private dialog = inject(MatDialog);
 
   ngOnInit(): void {
     // 
+  }
+
+  openModal() {
+    this.dialog.open(ModalSubject, {
+      panelClass: 'custom-modal-studyflow'
+    });
   }
 }
